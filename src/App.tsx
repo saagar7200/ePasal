ca7d644109624472b5005eb8ef8b761ebe6e1144
sagar <rsaagar7200@gmail.com>
2023-08-14 21:54:50 +0545

    Welcome to ePasal system

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="root">
+        <h2 className="title">Welcome to ePasal</h2>
+        <p className="subHead">powered by G-group</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
