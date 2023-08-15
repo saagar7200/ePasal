c605891565b90d4c09a5dfd326436c0b67b106ec
sagar <rsaagar7200@gmail.com>
2023-08-16 00:33:09 +0545

    add: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
 import { Box, Button, Heading, Text } from "@chakra-ui/react";
 import "./App.css";
 import { colors } from "./constants/theme/colors";
+import { useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-function App() {
+const App = () => {
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    navigate("/epasal/join-Us");
+  };
   return (
     <>
-      <Box className="root" h={"auto"} width={"100vw"}>
+      <Box border={"1px solid red"} className="root" h={"auto"} width={"100vw"}>
         <Heading>Welcome to ePasal</Heading>
         <Text>powered by G-group</Text>
-        <Button background={colors.primary.primary_01}>Join Us</Button>
+        <Button background={colors.primary.primary_01} onClick={onClick}>
+          Join Us
+        </Button>
+        <Routes>
+          <Route path={"/"} element={<Box>Welcome</Box>} />
+
+          <Route path={"/epasal/join-Us"} element={<Box>Page Not Found</Box>} />
+        </Routes>
       </Box>
     </>
   );
-}
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </ChakraProvider>
   </React.StrictMode>
 );
