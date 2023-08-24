024466c405b93526b9e8daa73c12c5da5f90947b
sagar <rsaagar7200@gmail.com>
2023-08-24 17:50:33 +0545

    test changes

--- src/UI/organisms/ProductCard.tsx
+++ src/UI/organisms/ProductCard.tsx
@@ -17,7 +17,7 @@ const ProductCard = () => {
     imageAlt: "Rear view of modern home with pool",
     beds: 3,
     baths: 2,
-    title: "Modern home in city center in the heart of historic Los Angeles",
+    title: "Modern home at city center in the heart of historic Los Angeles",
     formattedPrice: "$1,900.00",
     reviewCount: 34,
     rating: 4,
